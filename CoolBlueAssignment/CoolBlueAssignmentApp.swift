1007e04b1f95b03599c544a3cee7eda69fdc0eb4
samchen7910 <samchenjia@gmail.com>
2025-07-11 17:29:13 +0200

    Initial Commit

---
+++ CoolBlueAssignment/CoolBlueAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolBlueAssignmentApp.swift
+//  CoolBlueAssignment
+//
+//  Created by Tran Gia Huy on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CoolBlueAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoolBlueAssignmentTests/CoolBlueAssignmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoolBlueAssignmentTests.swift
+//  CoolBlueAssignmentTests
+//
+//  Created by Tran Gia Huy on 7/11/25.
+//
+
+import Testing
+@testable import CoolBlueAssignment
+
+struct CoolBlueAssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
